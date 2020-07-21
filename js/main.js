b1a36794a8e64ea1a548c030427cbf7c90b98e0d
priya <priya@siembramobile.com>
2020-07-21 13:24:49 +0530

    new changes

--- js/main.js
+++ js/main.js
@@ -49,3 +49,17 @@ function scrollFunction() {
     document.getElementById("top").style.display = "none";
   }
 }
+
+
+$('.counter-count').each(function () {
+        $(this).prop('Counter',0).animate({
+            Counter: $(this).text()
+        }, {
+            duration: 5000,
+            easing: 'swing',
+            step: function (now) {
+                $(this).text(Math.ceil(now));
+            }
+        });
+    });
+
--- js/owlCarousel.js
+++ js/owlCarousel.js
@@ -1,4 +1,4 @@
-$('.owl-carousel').owlCarousel({
+/*$('.owl-carousel').owlCarousel({
     loop: true,
     margin: 10,
     nav: true,
@@ -13,4 +13,30 @@ $('.owl-carousel').owlCarousel({
         items: 6
       }
     }
-  })
+  })*/
+jQuery(document).ready(function($) {
+                        "use strict";
+                        //  TESTIMONIALS CAROUSEL HOOK
+                        $('#customers-testimonials').owlCarousel({
+                            loop: true,
+                            center: true,
+                            items: 3,
+                            margin: 0,
+                            autoplay: true,
+                            dots:true,
+                            autoplayTimeout: 8500,
+                            smartSpeed: 450,
+                            responsive: {
+                              0: {
+                                items: 1
+                              },
+                              768: {
+                                items: 2
+                              },
+                              1170: {
+                                items: 3
+                              }
+                            }
+                        });
+                });
+
